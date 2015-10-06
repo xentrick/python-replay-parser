80106f2af6a0267df0e19050eda81a6d1b1dbc6c
Daniel Samuels <daniel.samuels1@gmail.com>
2015-10-06 22:29:49 +0100

    Add placeholder test script.

---
+++ utils/tests.py
@@ -0,0 +1,19 @@
+from rl_replay_parser import ReplayParser
+
+import os
+import unittest
+
+
+class TestReplayParser(unittest.TestCase):
+
+    def test_example_replays(self):
+        parser = ReplayParser()
+
+        for filename in os.listdir('example_replays'):
+            with open('example_replays/{}'.format(filename)) as f:
+                response = parser.parse(f)
+
+                self.assertIsInstance(response, dict)
+
+if __name__ == '__main__':
+    unittest.main()
