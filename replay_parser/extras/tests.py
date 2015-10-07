c10f01c6efa482e8abd1e962efb8a846398d0ab4
Daniel Samuels <daniel.samuels1@gmail.com>
2015-10-07 14:32:57 +0100

    Add simple tests for replay parser.

--- replay_parser/extras/tests.py
+++ replay_parser/extras/tests.py
@@ -1,19 +1,33 @@
-from ..replay_parser import ReplayParser
+from replay_parser import ReplayParser
 
-import os
+import struct
 import unittest
 
 
 class TestReplayParser(unittest.TestCase):
 
-    def test_example_replays(self):
+    def test_104_replay(self):
         parser = ReplayParser()
 
-        for filename in os.listdir('replay_parser/extras/example_replays'):
-            with open('replay_parser/extras/example_replays/{}'.format(filename)) as f:
-                response = parser.parse(f)
+        with open('extras/example_replays/1.04.replay') as f:
+            response = parser.parse(f)
+            self.assertIsInstance(response, dict)
+            self.assertEqual(response['header']['Id'], '0AB18BAB4CCE97201B7753A84B358D48')
 
-                self.assertIsInstance(response, dict)
+    def test_105_replay(self):
+        parser = ReplayParser()
+
+        with open('extras/example_replays/1.05.replay') as f:
+            response = parser.parse(f)
+            self.assertIsInstance(response, dict)
+            self.assertEqual(response['header']['Id'], '56E7708C45ED1CF3B9E51EBF1ADF4431')
+
+    def test_broken_replay(self):
+        parser = ReplayParser()
+
+        with open('extras/example_replays/broken.replay') as f:
+            with self.assertRaises(struct.error):
+                parser.parse(f)
 
 if __name__ == '__main__':
     unittest.main()
