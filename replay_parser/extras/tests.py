a7f2787c801f0100a2fe47c362556881fb88e949
Daniel Samuels <daniel.samuels1@gmail.com>
2015-10-07 14:42:22 +0100

    Attempt to fix folder location in tests.

--- replay_parser/extras/tests.py
+++ replay_parser/extras/tests.py
@@ -1,15 +1,20 @@
 from replay_parser import ReplayParser
 
+import os
 import struct
 import unittest
 
 
 class TestReplayParser(unittest.TestCase):
 
+    folder_path = '{}/example_replays/'.format(
+        os.path.dirname(os.path.realpath(__file__))
+    )
+
     def test_104_replay(self):
         parser = ReplayParser()
 
-        with open('extras/example_replays/1.04.replay') as f:
+        with open(self.folder_path + '1.04.replay') as f:
             response = parser.parse(f)
             self.assertIsInstance(response, dict)
             self.assertEqual(response['header']['Id'], '0AB18BAB4CCE97201B7753A84B358D48')
@@ -17,7 +22,7 @@ class TestReplayParser(unittest.TestCase):
     def test_105_replay(self):
         parser = ReplayParser()
 
-        with open('extras/example_replays/1.05.replay') as f:
+        with open(self.folder_path + '1.05.replay') as f:
             response = parser.parse(f)
             self.assertIsInstance(response, dict)
             self.assertEqual(response['header']['Id'], '56E7708C45ED1CF3B9E51EBF1ADF4431')
@@ -25,7 +30,7 @@ class TestReplayParser(unittest.TestCase):
     def test_broken_replay(self):
         parser = ReplayParser()
 
-        with open('extras/example_replays/broken.replay') as f:
+        with open(self.folder_path + 'broken.replay') as f:
             with self.assertRaises(struct.error):
                 parser.parse(f)
 
