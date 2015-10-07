3a3e9b26012ef698e9f7e1cbb7732712a9dd00bc
Daniel Samuels <daniel.samuels1@gmail.com>
2015-10-07 15:26:08 +0100

    Add `keyframes_missing` replay file.

--- replay_parser/extras/tests.py
+++ replay_parser/extras/tests.py
@@ -12,6 +12,10 @@ class TestReplayParser(unittest.TestCase):
     )
 
     def test_104_replay(self):
+        """
+        A replay from version 1.04.
+        """
+
         parser = ReplayParser()
 
         with open(self.folder_path + '1.04.replay') as f:
@@ -20,6 +24,10 @@ class TestReplayParser(unittest.TestCase):
             self.assertEqual(response['header']['Id'], '0AB18BAB4CCE97201B7753A84B358D48')
 
     def test_105_replay(self):
+        """
+        A replay from version 1.05.
+        """
+
         parser = ReplayParser()
 
         with open(self.folder_path + '1.05.replay') as f:
@@ -28,9 +36,27 @@ class TestReplayParser(unittest.TestCase):
             self.assertEqual(response['header']['Id'], '56E7708C45ED1CF3B9E51EBF1ADF4431')
 
     def test_broken_replay(self):
+        """
+        This replay file was purposefully broken by deleting a large portion
+        of the data.
+        """
+
         parser = ReplayParser()
 
         with open(self.folder_path + 'broken.replay') as f:
+            with self.assertRaises(struct.error):
+                parser.parse(f)
+
+    def test_keyframes_missing_replay(self):
+        """
+        For some reason, this replay is missing the key frames from when goals
+        were scored, so that data is not available to a parser. This is a good
+        test to ensure the parser can handle odd scenarios.
+        """
+
+        parser = ReplayParser()
+
+        with open(self.folder_path + 'keyframes_missing.replay') as f:
             with self.assertRaises(struct.error):
                 parser.parse(f)
 
