449e19def80c3aecea6d65a6cbc5979dba0c7094
Daniel Samuels <daniel.samuels1@gmail.com>
2015-10-07 16:08:37 +0100

    Update setup.py.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-from setuptools import setup
+from setuptools import find_packages, setup
 
 from replay_parser import VERSION
 
@@ -10,14 +10,15 @@ setup(
     url="https://github.com/danielsamuels/rocket-league-replay-parser",
     author="Daniel Samuels",
     author_email="daniel.samuels1@gmail.com",
-    license="BSD",
+    packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     description='Parser for Rocket League replay files.',
     classifiers=[
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
-
+        'License :: OSI Approved :: GNU General Public License (GPL)',
     ],
 )
